8bb7d8881611efb286b55408aa97ff3ba91cbda2
JMLee1231 <jlee1231@calpoly.edu>
2024-11-18 18:29:32 -0800

    Update group_tests.py
    recommit

--- group_tests.py
+++ group_tests.py
@@ -6,7 +6,7 @@ class MyTestCase(unittest.TestCase):
     def test_group_test_uneven(self):
         expected = [[1,2,3],[4,5,6],[7]]
         actual = group.groups_of_3([1,2,3,4,5,6,7])
-        self.assertEqual(expected, actual)
+        self.assertEqual(expected, actual) 
 
     def test_group_test_triple(self):
         expected = [[1, 2, 3], [4, 5, 6]]
